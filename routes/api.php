d0955c1fb7fa8bc4d47ba0a89d845dc528fcb1c7
Ahmet <ahmetkorkmaz3453@gmail.com>
2021-03-16 18:43:22 +0300

    Route prettier

--- routes/api.php
+++ routes/api.php
@@ -20,14 +20,11 @@ Route::prefix('auth')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
 });
 
-Route::middleware('auth:api')->group(function () {
-    Route::prefix('auth')->group(function () {
-        Route::get('me', [AuthController::class, 'me']);
-        Route::post('logout', [AuthController::class, 'logout']);
-        Route::post('refresh', [AuthController::class, 'refresh']);
-    });
-});
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+    'middleware' => 'auth:api',
+    'prefix' => 'auth'
+], function () {
+    Route::get('me', [AuthController::class, 'me']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
 });
